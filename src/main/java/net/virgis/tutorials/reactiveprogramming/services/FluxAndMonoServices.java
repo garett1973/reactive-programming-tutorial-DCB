f23963b503b197168d713facfec05db16af786e3
garett1973 <hobby@virgis.net>
2022-09-01 18:37:24 +0300

    Operators

--- src/main/java/net/virgis/tutorials/reactiveprogramming/services/FluxAndMonoServices.java
+++ src/main/java/net/virgis/tutorials/reactiveprogramming/services/FluxAndMonoServices.java
@@ -3,7 +3,9 @@ package net.virgis.tutorials.reactiveprogramming.services;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class FluxAndMonoServices {
 
@@ -11,8 +13,63 @@ public class FluxAndMonoServices {
         return Flux.fromIterable(List.of("Apple", "Banana", "Orange", "Pear")).log();
     }
 
+    public Flux<String> fruitsFluxMap() {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange", "Pear"))
+                .map(String::toUpperCase)
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFilter(int length) {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange", "Pear"))
+                .filter(s -> s.length() > length)
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFilterMap(int length) {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange", "Pear"))
+                .filter(s -> s.length() > length)
+                .map(String::toUpperCase)
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFlatMap() {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange", "Pear"))
+                .flatMap(s -> Flux.just(s.split("")))
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFlatMapAsync() {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange", "Pear"))
+                .flatMap(s -> Flux.just(s.split(""))
+                .delayElements(Duration.ofMillis(
+                        new Random().nextInt(1000)
+                ))) // delay each element by a random amount of time
+                .log();
+    }
+
+    public Flux<String> fruitsFluxConcatMap() {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange", "Pear"))
+                .concatMap(s -> Flux.just(s.split("")))
+                .delayElements(Duration.ofMillis(
+                        new Random().nextInt(1000)
+                )) // delay each element by a random amount of time
+                .log();
+    }
+
+    public Flux<String> fruitMonoFlatMapMany() {
+        return Mono.just("Mango")
+                .flatMapMany(s -> Flux.just(s.split("")))
+                .log();
+    }
+
     public Mono<String> fruitMono() {
         return Mono.just("Mango").log();
+    }
+
+    public Mono<List<String>> fruitMonoFlatMap() {
+        return Mono.just("Mango")
+                .flatMap(s -> Mono.just(List.of(s.split(""))))
+                .log();
     }
 
     public static void main(String[] args) {
--- src/test/java/net/virgis/tutorials/reactiveprogramming/services/FluxAndMonoServicesTest.java
+++ src/test/java/net/virgis/tutorials/reactiveprogramming/services/FluxAndMonoServicesTest.java
@@ -3,6 +3,8 @@ package net.virgis.tutorials.reactiveprogramming.services;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoServicesTest {
@@ -27,6 +29,120 @@ class FluxAndMonoServicesTest {
 
         StepVerifier.create(fruitMono)
                 .expectNext("Mango")
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxMap() {
+
+        var fruitsFluxMap = fluxAndMonoServices.fruitsFluxMap();
+        StepVerifier.create(fruitsFluxMap)
+                .expectNext("APPLE")
+                .expectNext("BANANA")
+                .expectNext("ORANGE")
+                .expectNext("PEAR")
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxFilter() {
+        var fruitsFluxFilter = fluxAndMonoServices.fruitsFluxFilter(5);
+        StepVerifier.create(fruitsFluxFilter)
+                .expectNext("Banana")
+                .expectNext("Orange")
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxFilterMap() {
+        var fruitsFluxFilterMap = fluxAndMonoServices.fruitsFluxFilterMap(5);
+        StepVerifier.create(fruitsFluxFilterMap)
+                .expectNext("BANANA")
+                .expectNext("ORANGE")
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxFlatMap() {
+        var fruitsFluxFlatMap = fluxAndMonoServices.fruitsFluxFlatMap();
+        StepVerifier.create(fruitsFluxFlatMap)
+                .expectNext("A")
+                .expectNext("p")
+                .expectNext("p")
+                .expectNext("l")
+                .expectNext("e")
+                .expectNext("B")
+                .expectNext("a")
+                .expectNext("n")
+                .expectNext("a")
+                .expectNext("n")
+                .expectNext("a")
+                .expectNext("O")
+                .expectNext("r")
+                .expectNext("a")
+                .expectNext("n")
+                .expectNext("g")
+                .expectNext("e")
+                .expectNext("P")
+                .expectNext("e")
+                .expectNext("a")
+                .expectNext("r")
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxFlatMapAsync() {
+        var fruitsFluxFlatMapAsync = fluxAndMonoServices.fruitsFluxFlatMapAsync();
+        StepVerifier.create(fruitsFluxFlatMapAsync)
+                .expectNextCount(21)
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitMonoFlatMap() {
+        var fruitMonoFlatMap = fluxAndMonoServices.fruitMonoFlatMap();
+        StepVerifier.create(fruitMonoFlatMap)
+                .expectNext((List<String>) List.of("M", "a", "n", "g", "o"))
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxConcatMap() {
+        var fruitsFluxConcatMap = fluxAndMonoServices.fruitsFluxConcatMap();
+        StepVerifier.create(fruitsFluxConcatMap)
+                .expectNext("A")
+                .expectNext("p")
+                .expectNext("p")
+                .expectNext("l")
+                .expectNext("e")
+                .expectNext("B")
+                .expectNext("a")
+                .expectNext("n")
+                .expectNext("a")
+                .expectNext("n")
+                .expectNext("a")
+                .expectNext("O")
+                .expectNext("r")
+                .expectNext("a")
+                .expectNext("n")
+                .expectNext("g")
+                .expectNext("e")
+                .expectNext("P")
+                .expectNext("e")
+                .expectNext("a")
+                .expectNext("r")
+                .verifyComplete();
+    }
+
+    @Test
+    void fruitMonoFlatMapMany() {
+        var fruitMonoFlatMapMany = fluxAndMonoServices.fruitMonoFlatMapMany();
+        StepVerifier.create(fruitMonoFlatMapMany)
+                .expectNext("M")
+                .expectNext("a")
+                .expectNext("n")
+                .expectNext("g")
+                .expectNext("o")
                 .verifyComplete();
     }
 }
